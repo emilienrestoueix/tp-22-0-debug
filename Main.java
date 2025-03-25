38d86fe058542d265cd0777f3fad39a0cfa97b5f
Restoueix Emilien <restouee@3il.fr>
2025-03-25 14:50:44 +0100

    tp1 done

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Problème 1
         int[] numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number at index " + i + ": " + numbers[i]);
         }
 
@@ -14,28 +14,36 @@ public class Main {
         int count = 10;
         while (count > 0) {
             System.out.println("Count: " + count);
-            count += 2;
+            count -= 2;
         }
 
         // Problème 4
         String message = "Bonjour";
         System.out.println("Longueur du message : " + message.length());
-        char character = message.charAt(10);
-        System.out.println("Caractère à l'indice 10 : " + character);
+        try {
+            char character = message.charAt(message.length()-1);
+            System.out.println("Caractère à l'indice " + message.length() + " : " + character);
+        } catch (Exception e){
+            throw new Exception("Index de charactere incorrect");
+        }
 
         // Problème 5
         String str1 = "Hello";
         String str2 = new String("Hello");
-        System.out.println("Comparaison de chaînes : " + (str1 == str2));
+        System.out.println("Comparaison de chaînes : " + (str1.equals(str2)));
 
         // Problème 6
-        Main mainObj = null;
+        Main mainObj = new Main();
         mainObj.method();
     }
 
     // Méthode pour le problème de division par zéro
     public static int calculateValue(int a, int b) {
-        return a / b;
+        if (a == 0 || b == 0) {
+            return 0;
+        } else {
+            return a / b;
+        }
     }
 
     // Méthode pour le problème d'utilisation incorrecte d'une méthode de classe
